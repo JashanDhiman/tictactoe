79b42e271d73dfeaafd12aa6b85fa910f495a21b
jashan <jashandhiman.hulk@gmail.com>
2021-07-23 17:54:37 +0530

    winners and turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,24 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return square;
-      });
-    });
-    setIsXNext(prev => !prev);
-  };
-  const renderSquareClick = position => {
+const Board = ({ board, handleSquareClick }) => {
+  const renderSquare = position => {
     return (
       <Square
         value={board[position]}
@@ -30,19 +14,19 @@ const Board = () => {
   return (
     <div className="board">
       <div className="board-row">
-        {renderSquareClick(0)}
-        {renderSquareClick(1)}
-        {renderSquareClick(2)}
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        {renderSquareClick(3)}
-        {renderSquareClick(4)}
-        {renderSquareClick(5)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        {renderSquareClick(6)}
-        {renderSquareClick(7)}
-        {renderSquareClick(8)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
